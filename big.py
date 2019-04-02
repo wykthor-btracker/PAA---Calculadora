c306e9b2b93afe157e15129fd1251d36aaf37de3
Wykthor-Btracker <wykthor.g@wgvale.com>
2019-04-02 20:26:04 -0300

    coisas

--- big.py
+++ big.py
@@ -1,19 +1,19 @@
 class BigNumber:
     def __init__(self, num):
         self.num = num
-	assert(isinstance(num,str))
-	try:
-		float(num)
-	except ValueError as e:
-		raise Exception("Valor inválido recebido! {} tem caracteres não esperados. 
-				Apenas números ou . esperados.\nErro:{}".format(num,e))
-	except Exception as e:
-		raise Exception("Exceção inesperada: {}".format(e))
+        assert(isinstance(num,str))
+        try:
+            float(num)
+        except ValueError as e:
+	    raise Exception("Valor inválido recebido! {} tem caracteres não esperados. 
+                             Apenas números ou . esperados.\nErro:{}".format(num,e))
+        except Exception as e:
+            raise Exception("Exceção inesperada: {}".format(e))
     def __str__(self):
         return self.num
 
     def __repr__(self):
-	return "BigNumber({})".format(num)
+        return "BigNumber({})".format(num)
 
 def main():
 	linha = input('')
