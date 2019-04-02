7cd67d55f3d4a66b732d39ebcc1377fce2076d6f
Wykthor <wykthor.g@wgvale.com>
2019-04-02 20:21:23 -0300

    Update big.py
    Todo objeto em python tem alguns métodos que já vem pré-definidos, por exemplo, __str__ e __repr__ são métodos usados para representar o objeto informal e formalmente, respectivamente, este serve para descrever o jeito que a instância foi inicializada, e aquele serve pra fazer com que o objeto seja legível.
    
    Na inicialização, foram definidas formas de garantir que o valor passado seja capaz de ser utilizado como um número, ou seja, contenha apenas caracteres válidos para um número, e que o valor seja do tipo string.

--- big.py
+++ big.py
@@ -1,14 +1,24 @@
 class BigNumber:
     def __init__(self, num):
         self.num = num
+	assert(isinstance(num,str))
+	try:
+		float(num)
+	except ValueError as e:
+		raise Exception("Valor inválido recebido! {} tem caracteres não esperados. 
+				Apenas números ou . esperados.\nErro:{}".format(num,e))
+	except Exception as e:
+		raise Exception("Exceção inesperada: {}".format(e))
+    def __str__(self):
+        return self.num
 
-    def mostrar(self):
-        print(self.num)
-
+    def __repr__(self):
+	return "BigNumber({})".format(num)
 
 def main():
-	linha = input('');
-	a = BigNumber(linha);
-	a.mostrar();
-
-main();
+	linha = input('')
+	a = BigNumber(linha)
+	print(a)
+	
+if __name__ == "__main__":
+	main()
