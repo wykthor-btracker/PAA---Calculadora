1a35e24449abe4b37f64b9908b146f394c3be73f
JessyMota <jsm.universitaria@gmail.com>
2019-04-08 17:49:29 -0300

    Update multi_chain_matrix.py
    correct algorithm

--- multi_chain_matrix.py
+++ multi_chain_matrix.py
@@ -1,30 +1,36 @@
-gk = lambda i,j:str(i)+','+str(j)
-MAX = 1000000
+def f(i,j):
+    return (str(i)+","+str(j))
 
-def memoized_matrix_chain(p):
-    n = len(p)-1
-    m = {}
+def matrix_chain_order(p): #a entrada p é caracterizada como o exemplo: matrizes 50x20, 20x1, 1x10, 10x100= (50, 20, 1, 10, 100)
+    n = len(p)-1 # numero de matrizes a serem multiplicadas
+    m, s = {}, {} # m[i,j] guardará o menor numero de operacoes para se multiplicar as matrizes de i a j;
     for i in range(1, n+1):
-        for j in range (i, n+1):
-            m[gk(i, j)] = MAX
-    return lookup_chain(m, p, 1, n)
+        m[f(i, i)] = 0 #salva em m[i,i] o numero de operacoes com uma unica matriz = 0
+    for l in range(2, n+1): # inicia com cadeia de duas matrizes: l=2 e vai aumentando
+        for i in range(1, n-l+2):
+            j = i+l-1
+            m[f(i, j)] = 100000000 
+            for k in range(i, j): # obtencao de m[i,j] 
+                q = m[f(i, k)]+m[f(k+1, j)]+p[i-1]*p[k]*p[j] #aqui entra programacao dinamica, pois varios valores da arvore de recursao ja foram calculados e sao trazidos de m
+                if q<m[f(i, j)]:
+                    m[f(i, j)] = q #obtencao de m otimo para multiplicacao da cadeia de matrizes de i a j
+                    s[f(i, j)] = k #onde ocorreu a divisao otima da cadeia de matrizes de i a j
+    return m, s
 
-def lookup_chain(m, p, i, j):
-    if m[gk(i, j)] < MAX:
-        return m[gk(i, j)]
+def get_optimal_order(s, i, j): 
+    res = ""
     if i == j:
-        m[gk(i, j)] = 0
+        return ("A"+str(j))
     else:
-        for k in range(i, j):
-            q = lookup_chain(m, p, i, k) + lookup_chain(m, p, k+1, j) + p[i-1]*p[k]*p[j]
-            if q < m[gk(i, j)]:
-                m[gk(i, j)] = q
-    return m[gk(i, j)]
+        res += "("
+        res += get_optimal_order(s, i, s[f(i, j)])
+        res += get_optimal_order(s, s[f(i, j)]+1, j)
+        res +=  ")"
+        return res
 
 
 
 p = [50,20,1,10,100]
-
-
-print("minimum number of operations:", memoized_matrix_chain(p))
-#Next step: what was the order of the multiplication?
+m, s = matrix_chain_order(p)
+print ("number operations：", m[f(1, len(p)-1)])
+print ("best order of matrix multiplication:", get_optimal_order(s, 1, len(p)-1))
