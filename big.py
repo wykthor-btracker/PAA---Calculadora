d204efa4d19564ffe8af41bac8d6a8ac26a354f3
Wykthor <wykthor.g@wgvale.com>
2019-04-08 15:07:23 -0300

    Extração de vários métodos da função __add__  para deixá-la focada no seu intuito

--- big.py
+++ big.py
@@ -23,9 +23,59 @@ class BigNumber:
     def __len__(self):
         return len(self.num)
 
+    def __add__(self,other):
+        if not isinstance(other, type(self)):
+            raise Exception("Adição mal feita, esperava um objeto do tipo {}, tipo {} recebido".format(BigNumber,
+                                                                                                       type(other)))
+        maior = lambda values: reduce(lambda bigNumA, bigNumB: bigNumA if len(bigNumA) >= len(bigNumB) else bigNumB,
+                                      values)
+        menor = lambda values: reduce(lambda bigNumA, bigNumB: bigNumA if len(bigNumA) < len(bigNumB) else bigNumB,
+                                      values)
+
+        indexTuples, longestNumber, shortestNumber = self._initSum(maior, menor, other)
+
+        res = self._SumSubsets(indexTuples, longestNumber, shortestNumber)
+
+        end = self._JoinSumWithLeftover(longestNumber, res)
+        return BigNumber(end)
+
+    @staticmethod
+    def _JoinSumWithLeftover(longestNumber, res):
+        leftover = longestNumber[:-len(res)]
+        result = "".join(map(lambda x: str(x), res))
+        end = leftover + result
+        return end
+
+    @staticmethod
+    def _SumSubsets(indexTuples, longestNumber, shortestNumber):
+        carry = 0
+        res = []
+        for indexA, indexB in indexTuples:
+            unitSum = int(longestNumber[indexA]) + int(shortestNumber[indexB])
+            if carry:
+                unitSum += carry
+                carry = 0
+            if unitSum > 9:
+                unitSum = unitSum % 10
+                carry = 1
+            res.insert(0, unitSum)
+        return res
+
+    def _initSum(self, maior, menor, other):
+        cutOff = min(len(self.num), len(other.num))
+        longestNumber = maior([self.num, other.num])
+        shortestNumber = menor([self.num, other.num])
+        indexListLargest = list(reversed(range(len(longestNumber))))[:cutOff]
+        indexListSmallest = list(reversed(range(cutOff)))
+        indexTuples = [(indexListLargest[index], indexListSmallest[index]) for index in range(cutOff)]
+        return indexTuples, longestNumber, shortestNumber
+
 
 def main():
-    pass
+    x, y = 1230000,25000000
+    a, b = BigNumber(str(x)),BigNumber(str(y))
+    print("{} + {} = {}".format(a, b, a+b))
+    print("{} + {} = {}".format(x, y, x+y))
 
 
 if __name__ == "__main__":
