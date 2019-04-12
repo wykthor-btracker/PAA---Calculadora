c19a2d4b0c118041b4847ed4e138478ca6b1932e
JessyMota <jsm.universitaria@gmail.com>
2019-04-12 12:41:35 -0300

    Update big.py

--- big.py
+++ big.py
@@ -145,25 +145,25 @@ class BigNumber:
         B = int(self._zeroPad(str(k - ac - bd), BZeroPadding, False))
         return A + B + bd
     
-    def root(x, n): 
+def root(x, n): 
 
-        x = float(x) 
-        n = int(n) 
-        if (x >= 0 and x <= 1): 
-            menor = x 
-            maior = 1
+    x = float(x) 
+    n = int(n) 
+    if (x >= 0 and x <= 1): 
+        menor = x 
+        maior = 1
+    else: 
+        menor = 1
+        maior = x         
+    e = 0.00000001 
+    chute = (menor + maior) / 2
+    while abs(chute ** n - x) >= e: 
+        if chute ** n > x: 
+            maior = chute 
         else: 
-            menor = 1
-            maior = x         
-        e = 0.00000001 
+            menor = chute
         chute = (menor + maior) / 2
-        while abs(chute ** n - x) >= e: 
-            if chute ** n > x: 
-                maior = chute 
-            else: 
-                menor = chute
-            chute = (menor + maior) / 2
-        return chute
+    return round(chute, 2)
 
 def exp(x, n):
     if n == 1:
@@ -171,6 +171,113 @@ def exp(x, n):
     if n % 2 == 0:
         return exp(x * x, n // 2)
     return x * exp(x * x, n // 2)
+
+def check(str1, str2): 
+  
+    n1 = len(str1)  
+    n2 = len(str2) 
+   
+    if (n1 < n2): 
+        return True
+    if (n2 < n1): 
+        return False
+   
+    for i in range(n1): 
+        if (str1[i] < str2[i]): 
+            return True
+        elif (str1[i] > str2[i]): 
+            return False
+   
+    return False
+
+def subt(str1, str2): 
+  
+
+    if (check(str1, str2)): 
+        aux = str1 
+        str1 = str2 
+        str2 = aux 
+   
+   
+    str3 = "" 
+   
+    n1 = len(str1)  
+    n2 = len(str2) 
+   
+ 
+    str1= str1[::-1] 
+    str2 = str2[::-1] 
+  
+    carry = 0
+
+
+    for i in range(n2): 
+      
+
+           
+        sub = ((ord(str1[i])-ord('0'))-(ord(str2[i])-ord('0'))-carry) 
+          
+        if (sub < 0): 
+          
+            sub = sub + 10
+            carry = 1
+              
+        else: 
+            carry = 0
+  
+        str3 = str3+str(sub ) 
+          
+    
+    for i in range(n2,n1): 
+      
+        sub = ((ord(str1[i])-ord('0')) - carry) 
+           
+      
+        if (sub < 0): 
+          
+            sub = sub + 10
+            carry = 1
+          
+        else: 
+            carry = 0
+               
+        str3 = str3+str(sub) 
+   
+  
+    str3= str3[::-1] 
+   
+    return str3 
+
+
+
+
+def matrix_chain_order(p): #a entrada p é caracterizada como o exemplo: matrizes 50x20, 20x1, 1x10, 10x100= (50, 20, 1, 10, 100)
+    n = len(p)-1 # numero de matrizes a serem multiplicadas
+    m, s = {}, {} # m[i,j] guardará o menor numero de operacoes para se multiplicar as matrizes de i a j;
+    for i in range(1, n+1):
+        m[(i, i)] = 0 #salva em m[i,i] o numero de operacoes com uma unica matriz = 0
+    for l in range(2, n+1): # inicia com cadeia de duas matrizes: l=2 e vai aumentando
+        for i in range(1, n-l+2):
+            j = i+l-1
+            m[(i, j)] = 100000000 
+            for k in range(i, j): # obtencao de m[i,j] 
+                q = m[(i, k)]+m[(k+1, j)]+p[i-1]*p[k]*p[j] #aqui entra programacao dinamica, pois varios valores da arvore de recursao ja foram calculados e sao trazidos de m
+                if q<m[(i, j)]:
+                    m[(i, j)] = q #obtencao de m otimo para multiplicacao da cadeia de matrizes de i a j
+                    s[(i, j)] = k #onde ocorreu a divisao otima da cadeia de matrizes de i a j
+    return m, s
+
+def get_optimal_order(s, i, j): 
+    res = ""
+    if i == j:
+        return ("A"+str(j))
+    else:
+        res += "("
+        res += get_optimal_order(s, i, s[(i, j)])
+        res += get_optimal_order(s, s[(i, j)]+1, j)
+        res +=  ")"
+        return res
+
 
 def main():
     x, y = 521515, 51252125
@@ -180,6 +287,28 @@ def main():
     print("{} é igual a {}? {}".format(a, b, a==b))
     print("minha mult {}\nmult python {}".format(a*b,x*y))
 
+    #Numeros para subtraçao#
+    n1 = "1230434"
+    n2 = "12303940394"
+    print("subtração: ",subt(n1, n2)) 
 
+    #Numeros para raiz#
+    c = 4
+    d = 2
+    print("Raiz com radicando", c, "e indice", d, " é:",root(c, d))
+    #Numeros para exponeciacao#
+    e = 3
+    f = 2
+    print("exponenciação de ", e, "elevado a", f, " é: ",exp(e, f))
+
+    #Multiplicação de Cadeia de matrizes
+    p = [50,20,1,10,100]
+    m, s = matrix_chain_order(p)
+    print ("number operations：", m[(1, len(p)-1)])
+    print ("best order of matrix multiplication:", get_optimal_order(s, 1, len(p)-1))
+
+
+    
 if __name__ == "__main__":
     main()
+
